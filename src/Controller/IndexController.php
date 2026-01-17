851169fe52903942645d3bbd25af29f2640a558c
oumaimazaim30 <oumaima.zaim30gmail.com>
2026-01-17 14:36:17 +0100

    HomeConrtroller

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,13 +4,17 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
-    #[Route(path: '/index', name: 'app_index')]
-    public function index(): Response
+    #[Route(
+        path: '/index/{name}', 
+        name: 'app_index', 
+        requirements: ['name' => '\d+'] 
+    )]
+    public function index(string $name = "Anonymous"): Response
     {
-        return new Response(content: "Hello World !");
+        return new Response(content: "Hello " . $name . " !");
     }
 }
